1db022b418373ed9e2ac79607de2f55d31f4ba73
unknown <Asus@Asus-Bilgisayar.(none)>
2014-06-22 22:59:54 +0300

    refactor

--- src/main/java/tr/com/serkanozal/jillegal/service/ServiceManager.java
+++ src/main/java/tr/com/serkanozal/jillegal/service/ServiceManager.java
@@ -11,6 +11,8 @@ import tr.com.serkanozal.jillegal.compiler.service.ClassCompilerService;
 import tr.com.serkanozal.jillegal.compiler.service.ClassCompilerServiceFactory;
 import tr.com.serkanozal.jillegal.instrument.service.InstrumentService;
 import tr.com.serkanozal.jillegal.instrument.service.InstrumentServiceFactory;
+import tr.com.serkanozal.jillegal.offheap.memory.DirectMemoryService;
+import tr.com.serkanozal.jillegal.offheap.memory.DirectMemoryServiceFactory;
 import tr.com.serkanozal.jillegal.offheap.service.OffHeapService;
 import tr.com.serkanozal.jillegal.offheap.service.OffHeapServiceFactory;
 
@@ -18,6 +20,10 @@ public class ServiceManager {
 
 	private ServiceManager() {
 		
+	}
+	
+	public static DirectMemoryService getDirectMemoryService() {
+		return DirectMemoryServiceFactory.getDirectMemoryService();
 	}
 	
 	public static InstrumentService getInstrumentService() {
