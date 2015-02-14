170ec32fceb108ea0a08d3b0455a104465a94cda
unknown <Asus@Asus-Bilgisayar.(none)>
2015-02-14 15:11:53 +0200

    jillegal impl for version 3.0-SNAPSHOT

--- src/main/java/tr/com/serkanozal/jillegal/offheap/collection/map/OffHeapJudyHashMap.java
+++ src/main/java/tr/com/serkanozal/jillegal/offheap/collection/map/OffHeapJudyHashMap.java
@@ -339,10 +339,10 @@ public class OffHeapJudyHashMap<K, V> extends AbstractMap<K, V> implements OffHe
 
 //		volatile long keyAddress;
 //		volatile long valueAddress;
-		K key;
-		V value;
-		JudyEntry<K, V> prev;
-		JudyEntry<K, V> next;
+		volatile K key;
+		volatile V value;
+		volatile JudyEntry<K, V> prev;
+		volatile JudyEntry<K, V> next;
 		
 		@Override
 		public K getKey() {
@@ -430,7 +430,7 @@ public class OffHeapJudyHashMap<K, V> extends AbstractMap<K, V> implements OffHe
 	@OffHeapIgnoreInstrumentation
 	static class JudyIntermediateNode<K, V> extends JudyNode<K, V> {
 
-		JudyNode<K, V>[] children;
+		volatile JudyNode<K, V>[] children;
 
 		@SuppressWarnings("unchecked")
 		@Override
@@ -538,7 +538,7 @@ public class OffHeapJudyHashMap<K, V> extends AbstractMap<K, V> implements OffHe
 	@OffHeapIgnoreInstrumentation
 	static class JudyLeafNode<K, V> extends JudyNode<K, V> {
 
-		JudyEntry<K, V>[] entries;
+		volatile JudyEntry<K, V>[] entries;
 
 		@SuppressWarnings("unchecked")
 		void init() {
