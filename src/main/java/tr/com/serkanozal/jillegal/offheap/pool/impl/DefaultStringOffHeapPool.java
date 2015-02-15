e49efdd6f35e6ff0536a7408267718a6fd23a1fd
unknown <Asus@Asus-Bilgisayar.(none)>
2015-02-15 15:52:49 +0200

    jillegal impl for version 3.0-SNAPSHOT

--- src/main/java/tr/com/serkanozal/jillegal/offheap/pool/impl/DefaultStringOffHeapPool.java
+++ src/main/java/tr/com/serkanozal/jillegal/offheap/pool/impl/DefaultStringOffHeapPool.java
@@ -96,7 +96,7 @@ public class DefaultStringOffHeapPool extends BaseOffHeapPool<String, StringOffH
 			usedSegmentCount = 0;
 			inUseSegmentAddress = directMemoryService.allocateMemory(totalSegmentCount);
 			sampleStr = "";
-			sampleHeader = directMemoryService.getLong(sampleStr, 0L);
+			sampleHeader = 0x00000003; // | directMemoryService.getLong(sampleStr, 0L);
 			init();
 			makeAvaiable();
 		}
