bb6a505d7f9c38bbb0fa5da3e2b27c8e632cb864
unknown <Asus@Asus-Bilgisayar.(none)>
2015-02-15 16:18:19 +0200

    jillegal impl for version 3.0-SNAPSHOT

--- src/main/java/tr/com/serkanozal/jillegal/offheap/memory/DirectMemoryServiceImpl.java
+++ src/main/java/tr/com/serkanozal/jillegal/offheap/memory/DirectMemoryServiceImpl.java
@@ -164,13 +164,58 @@ public class DirectMemoryServiceImpl implements DirectMemoryService {
     	if (rootObj == null) {
     		return;
     	}
-    	System.out.print("set object field >>> ");
-    	System.out.println(
-    			"rootObj class: " + rootObj.getClass() + ", " +
-    			"rootObj header: " + unsafe.getLong(rootObj, 0L) + ", " +
-    			"rootObj: " + JvmUtil.toHexAddress(addressOf(rootObj)) + ", " +
-    			"fieldOffset: " + JvmUtil.toHexAddress(fieldOffset) + ", " + 
-    			"fieldObj: " + JvmUtil.toHexAddress(addressOf(fieldObj)));
+//    	synchronized (rootObj) {
+    		long objAddress = JvmUtil.addressOf(rootObj);
+	    	long fieldAddress = objAddress + fieldOffset;
+	    	if (fieldObj == null) {
+	    		int referenceSize = JvmUtil.getReferenceSize();
+            	switch (referenceSize) {
+                 	case JvmUtil.ADDRESSING_4_BYTE:   
+                 		unsafe.putInt(fieldAddress, (int) JvmUtil.NULL);
+                 		break;
+                 	case JvmUtil.ADDRESSING_8_BYTE:
+                 		unsafe.putLong(fieldAddress, JvmUtil.NULL);
+                 		break;	
+                 	default:    
+                        throw new AssertionError("Unsupported reference size: " + referenceSize);
+            	}
+	    	}
+	    	else {
+//	    		synchronized (fieldObj) {
+	    			long fieldObjAddress = JvmUtil.toJvmAddress(JvmUtil.addressOf(fieldObj));
+	    	      	if (fieldAddress != JvmUtil.NULL && fieldAddress != JvmUtil.INVALID_ADDRESS) {
+	    	    		switch (JvmUtil.getAddressSize()) {
+	    		            case JvmUtil.SIZE_32_BIT:
+	    		            	putAsIntAddress(fieldAddress, fieldObjAddress);
+	    		                break;
+	    		            case JvmUtil.SIZE_64_BIT:
+	    		            	int referenceSize = JvmUtil.getReferenceSize();
+	    		            	switch (referenceSize) {
+	    		                 	case JvmUtil.ADDRESSING_4_BYTE:   
+	    		                 		putAsIntAddress(fieldAddress, fieldObjAddress);
+	    		                 		break;
+	    		                 	case JvmUtil.ADDRESSING_8_BYTE:
+	    		                 		unsafe.putLong(fieldAddress, fieldObjAddress);
+	    		                 		break;	
+	    		                 	default:    
+	    		                        throw new AssertionError("Unsupported reference size: " + referenceSize);
+	    		            	}
+	    		            	break; 
+	    		            default:
+	    		                throw new AssertionError("Unsupported address size: " + JvmUtil.getAddressSize());
+	    	    		}      
+	    	    	}	
+//				}
+	    	}
+//    	}	
+//    	System.out.print("set object field >>> ");
+//    	System.out.println(
+//    			"rootObj class: " + rootObj.getClass() + ", " +
+//    			"rootObj header: " + unsafe.getLong(rootObj, 0L) + ", " +
+//    			"rootObj: " + JvmUtil.toHexAddress(addressOf(rootObj)) + ", " +
+//    			"fieldOffset: " + JvmUtil.toHexAddress(fieldOffset) + ", " + 
+//    			"fieldObj: " + JvmUtil.toHexAddress(addressOf(fieldObj)));
+/*    	
 //    	synchronized (rootObj) {
 	    	if (fieldObj == null) {
 	    		int referenceSize = JvmUtil.getReferenceSize();
@@ -210,7 +255,8 @@ public class DirectMemoryServiceImpl implements DirectMemoryService {
 		    		}      	
 //				}
 	    	}
-//    	}	
+//    	}
+	*/
     }
     
     @Override
